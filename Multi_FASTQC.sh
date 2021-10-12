687507a5ff65ca9373402922ec3653338af1eb3e
e1garcia <e1garcia@e2-w6420b-01.ib.cluster>
2021-10-12 12:53:48 -0400

    updated MULTIQC

--- Multi_FASTQC.sh
+++ Multi_FASTQC.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 
-#SBATCH --job-name=SgrMulti_fastqc
-#SBATCH -o SgrMulti_fastqc-%j.out
+#SBATCH --job-name=Multi_fastqc
+#SBATCH -o Multi_fastqc-%j.out
 #SBATCH -p main
-#SBATCH -c 4
+#SBATCH -c 32
 #SBATCH --mail-user=youremail
 #SBATCH --mail-type=begin
 #SBATCH --mail-type=END
@@ -51,12 +51,12 @@ module load container_env fastqc
 cd $2
 
 #run fastqc in parallel 
-ls *$1 | parallel "crun fastqc {}" &&
+ls *$1 | parallel -kj32 "crun fastqc {}" &&
 
 # run multiqc with specific report and subdirectory names
 crun multiqc . -n multiqc_report_$1.html -o ../Multi_FASTQC &&
 
 # move fastqc files to new subdirectory
-ls *fastqc.html | parallel "mv {} ../Multi_FASTQC" &&
-ls *fastqc.zip | parallel "mv {} ../Multi_FASTQC"
+ls *fastqc.html | parallel -kj 32 "mv {} ../Multi_FASTQC" &&
+ls *fastqc.zip | parallel -kj 32 "mv {} ../Multi_FASTQC"
 mv *out ../logs
