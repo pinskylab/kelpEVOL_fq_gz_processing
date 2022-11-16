ebe5bb00ddf32f4fa4a513bad0b412ef8a558569
cbird808 <cbird808@gmail.com>
2022-11-16 08:08:03 -0600

    update Multiqc_FASTQC

--- redoMultiQC.bash
+++ redoMultiQC.bash
@@ -21,14 +21,14 @@ sbatch /home/e1garcia/shotgun_PIRE/pire_fq_gz_processing/runMULTIQC.sbatch "$inD
 sbatch /home/e1garcia/shotgun_PIRE/pire_fq_gz_processing/runMULTIQC.sbatch "fq_fp1" "1st_fastp_report" &&
 
 # clmp
-sbatch /home/e1garcia/shotgun_PIRE/pire_fq_gz_processing/Multi_FASTQC.sh "fq_fp1_clmp" "fqc_clmp_report" &&
+sbatch /home/e1garcia/shotgun_PIRE/pire_fq_gz_processing/Multi_FASTQC.sh "fq_fp1_clmp" "fqc_clmp_report" "fq.gz" &&
 #sbatch /home/e1garcia/shotgun_PIRE/pire_fq_gz_processing/runMULTIQC.sbatch "fq_fp1_clmp" "fqc_clmp_report"
 
 # fp2
-sbatch /home/e1garcia/shotgun_PIRE/pire_fq_gz_processing/runMULTIQC.sbatch "fq_fp1_clmp_fp2" && "2nd_fastp_report"
+sbatch /home/e1garcia/shotgun_PIRE/pire_fq_gz_processing/runMULTIQC.sbatch "fq_fp1_clmp_fp2" "2nd_fastp_report" &&
 
 # fqscrn
-sbatch /home/e1garcia/shotgun_PIRE/pire_fq_gz_processing/runMULTIQC.sbatch "fq_fp1_clmp_fp2_fqscrn" && "fastq_screen_report"
+sbatch /home/e1garcia/shotgun_PIRE/pire_fq_gz_processing/runMULTIQC.sbatch "fq_fp1_clmp_fp2_fqscrn" "fastq_screen_report" &&
 
 # repair
-sbatch /home/e1garcia/shotgun_PIRE/pire_fq_gz_processing/runMULTIQC.sbatch "./fq_fp1_clmp_fp2_fqscrn_rprd" "fqc_rprd_report" &&
+sbatch /home/e1garcia/shotgun_PIRE/pire_fq_gz_processing/runMULTIQC.sbatch "./fq_fp1_clmp_fp2_fqscrn_rprd" "fqc_rprd_report" 
