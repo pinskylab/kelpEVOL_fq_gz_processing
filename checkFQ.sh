35e2327a9211b04dce60325ee90d4d2e98e53770
e1garcia <e1garcia@wahab-01.ib.cluster>
2023-08-04 17:56:20 -0400

    updating gridD  and checkFQ

--- checkFQ.sh
+++ checkFQ.sh
@@ -7,10 +7,11 @@
 #SBATCH --cpus-per-task=40
 #SBATCH --exclusive
 
+echo -e "############  checkFQ.sh  ############\nThis script checks the size as well as the\nzip and fastq formats in your .fq.gz files.\nFiles with\npotentially problematic size, zip\nor fastq formats will be printed in the log\n and respectively saved to:\n\nfiles_wDiff_sizes.txt\nfiles_w_alternative_zip_format.txt\nfiles_w_bad_fastq_format.txt\n\nYou might want to try to redownload\nthese .fq.gz files and/or check for\nissues with the formats\n######################################\n\n"
+
+# This scripts assumes that the tamucc_files.txt exits in the same directory as fq.gz files
 # Normal fq.gz files would have the following format: Blocked GNU Zip Format (BGZF; gzip compatible), block length xxxxxx
 # Alternative formats do not necessarily cause problems but they should be checked
-
-echo -e "############  checkFQ.sh  ############\nThis script checks the zip and fastq\nformat in your .fq.gz files. Files with\npotentially problematic zip or fastq\nformats will be printed here and\nrespectively saved to:\n\nfiles_w_alternative_zip_format.txt\nfiles_w_bad_fastq_format.txt\n\nYou might want to try to redownload\nthese .fq.gz files and/or check for\nissues with the formats\n######################################\n\n"
 
 # Directory with .fq.gz files
 FQGZDIR=$1
@@ -18,7 +19,35 @@ FQGZDIR=$1
 #Move to working dir
 cd $FQGZDIR
 
+# Compare size of tamucc and odu files
+# Check if the tamucc_files was created with ls or ls -lthr, and use that list to download files
+if [ ! -e "tamucc_files.txt" ]; then
+    echo "Cannot find 'tamucc_files.txt'. Place this file in the same directory as fq.gz files to  compare sizes. Skipping size check"
+fi
+
+NCOL=$(cat tamucc_files.txt | tail -n1 | awk '{print NF}')
+
+if [[ $NCOL -eq 9 ]]; then 
+	# Checking sizes of files from source and lpwd
+	echo -e "\nComparing file sizes from source (TAMUCC) and lpwd (ODU) with (grep -vf). This script assumes files have the same name"
+	# Create Intermediate files
+	cat tamucc_files.txt | grep '[zv]$' | tr -s " " | cut -d " " -f5,9 > tamucc_gzfiles
+	ls -lh *[zv] | tr -s " " | cut -d " " -f5,9 > odu_gzfiles
+
+	grep -vf tamucc_gzfiles odu_gzfiles > files_w_Diff_sizes.txt
+fi
+
+if [[ -s files_w_Diff_sizes.txt ]]; then
+        echo -e "\nFiles with different sizes detected. Offending file(s) printed in files_w_Diff_sizes.txt. Please check files_w_Diff_sizes and compare tamucc_files.txt with current downloaded data"
+        rm tamucc_gzfiles odu_gzfiles
+else 
+        echo -e "\nNo size mismatch in files was detected"
+	echo -e "\nNo size mismatch in files was detected" > files_w_Diff_sizes.txt
+        rm tamucc_gzfiles odu_gzfiles
+fi
+
 # Check the file format of each .fq.gz
+echo  -e "\nChekcing zip format in files"
 ls *fq.gz | parallel --no-notice -kj40 file {} > file_types.txt
 
 # Print files without Blocked GNU Zip Format
@@ -27,11 +56,11 @@ echo -e "Checking if any fq.gz file has another format other than <Blocked GNU Z
 mkdir -p fqgz_fileCheck
 echo "Files with an alternative zip format:"
 cat file_types.txt | grep -v 'Blocked GNU Zip Format'
-cat file_types.txt | grep -v 'Blocked GNU Zip Format'> files_w_alternative_zip_format 
+cat file_types.txt | grep -v 'Blocked GNU Zip Format'> files_w_alternative_zip_format.txt
 mv file_types.txt fqgz_fileCheck
 
 # Checking that files have proper FASTQ format (4 lines per sequence, where the 3rd is a "+")
-echo -e "\nChecking if files have proper FASTQ format (4 lines per sequence, where the 3rd is a +)\nIf all files have proper fastq format files_w_bad_fastq_format.txt will not be created\n"
+echo -e "\nChecking if files have proper FASTQ format (4 lines per sequence, where the 3rd is a +)\nResults will be printed in the log and in  files_w_bad_fastq_format.txt\n"
 
 ls *fq.gz | sed 's/\.fq\.gz//' | parallel --no-notice -kj40 "zcat {}.fq.gz | paste - - - - | cut -f3 | sort | uniq -c > fqgz_fileCheck/{}.third_column.txt"
 
@@ -46,6 +75,7 @@ for file in fqgz_fileCheck/*third_column.txt; do
 		echo "$BASE.fq.gz" >> files_w_bad_fastq_format.txt
         elif [ $line_count -eq 1 ]; then
             echo "File with proper fastq format: $BASE.fq.gz"
+	    echo "No issues detected. All files seem to have proper fastq format" > files_w_bad_fastq_format.txt
         else
             echo "Empty file: $BASE.fq.gz"
         fi
--- gridDownloader.sh
+++ gridDownloader.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --job-name=Wget
-#SBATCH -o Wget-%j.out
+#SBATCH --job-name=gridDown
+#SBATCH -o gridDown-%j.out
 #SBATCH --time=00:00:00
 #SBATCH --exclusive
 #SBATCH --ntasks=40
@@ -13,58 +13,85 @@
 # EXECUTE
 # First, locate the grid parent directory (which is just a website link) where the files will be downloaded from. Usually Sharon will post this in the species slack channel.
 # visually check in your browser that there is a file named tamucc_files.txt, which list the rest of the files in that directory
-# execute as "sbatch grid_wget.sh <outdir> <link-to-files>
+# There are 3 protocols. Try in this order, wget, wget1, wget2, curl, till you get not errors
+# (size errors are identified in this script and reported at the end of the log. Format errors are identified by checkFQ.sh)  
+# execute as "sbatch grid_wget.sh <outdir> <link-to-files> <wget|wget1|wget2|curl>
 
-module load parallel
+#module load parallel
 export SINGULARITY_BIND=/home/e1garcia  #odu
 
 # User defined variables
 OUTDIR=$1
 LINK=$(echo $2 | sed 's/\/$//')
+PROTOCOL=$3
+
+cd $OUTDIR
+
+if [[ "$PROTOCOL" == "wget" ]]; then
+        COMMAND="wget -c -P"
+	echo -e "Downloading using:\nprotocol= $PROTOCOL\ncommand= $COMMAND\n\n"
+elif [[ "$PROTOCOL" == "wget1" ]]; then
+        COMMAND="wget --secure-protocol=TLSv1_2 -c -P"    #yes, TLSv1_2 first
+        echo -e "Downloading using:\nprotocol= $PROTOCOL\ncommand= $COMMAND\n\n"
+elif [[ "$PROTOCOL" == "wget2" ]]; then
+        COMMAND="wget --secure-protocol=TLSv1_1 -c -P"
+	echo -e "Downloading using:\nprotocol= $PROTOCOL\ncommand= $COMMAND\n\n"
+elif [[ "$PROTOCOL" == "curl" ]]; then
+        COMMAND="curl -L -O -C - --output-dir"
+	echo -e "Downloading using:\nprotocol= $PROTOCOL\ncommand= $COMMAND\n\n"
+elif [[ -z "$PROTOCOL" ]]; then
+        COMMAND="wget -c -P"
+	echo -e "Protocol not specified. Downloading using:\nprotocol=wget\ncommand=$COMMAND\n\nIf you meant to run a different protocol please provite it as the third argument\nProtocol options= wget, wget1, wget2, curl. Try in this order\n\n."
+else
+        echo -e "Protocol not recognized. Options are: wget, wget1, wget2, curl. Please provide one of these as the third argument. Try them in the same order\n\n"
+fi 
+
 
 # downlownd the list of lifes
-wget -P $OUTDIR $LINK/tamucc_files.txt
+$COMMAND $OUTDIR $LINK/tamucc_files.txt
 NCOL=$(cat tamucc_files.txt | tail -n1 | awk '{print NF}')
 
-
+# Check if the tamucc_files was created with ls or ls -lthr, and use that list to download files
 if [[ $NCOL -eq 9 ]]
 then 
 # use that list to download all files in parallel.
-cat $OUTDIR/tamucc_files.txt | grep '^[-d]' | tr -s " " | cut -d " " -f9 | parallel --no-notice -kj20 wget -c -P $OUTDIR $LINK/{}
+cat $OUTDIR/tamucc_files.txt | grep '^[-d]' | tr -s " " | cut -d " " -f9 | parallel --no-notice -kj20 $COMMAND $OUTDIR $LINK/{}
 echo -e "\nFirst download completed.\n\nDownloading again in case partial downloads\n" 
 
 # Second download to check for partial downloads
-cat $OUTDIR/tamucc_files.txt | grep '^[-d]' | tr -s " " | cut -d " " -f9 | parallel --no-notice -kj20 wget -c -P $OUTDIR $LINK/{}
+cat $OUTDIR/tamucc_files.txt | grep '^[-d]' | tr -s " " | cut -d " " -f9 | parallel --no-notice -kj20 $COMMAND $OUTDIR $LINK/{}
 echo -e "\nSecond download completed" 
 
 # Checking sizes of files from source and lpwd
-echo -e "\nComparing file sizes from source and lpwd with (grep -vf)"
+#echo -e "\nComparing file sizes from source and lpwd with (grep -vf)"
 
 cd $OUTDIR
-cat tamucc_files.txt | grep '[zv]$' | tr -s " " | cut -d " " -f5,9 > tamucc_gzfiles
-ls -lh *[zv] | tr -s " " | cut -d " " -f5,9 > odu_gzfiles
 
-grep -vf tamucc_gzfiles odu_gzfiles > files_wDiff_sizes
+#Update August 2023, Size comparison is not done by checkFQ.sh
+#cat tamucc_files.txt | grep '[zv]$' | tr -s " " | cut -d " " -f5,9 > tamucc_gzfiles
+#ls -lh *[zv] | tr -s " " | cut -d " " -f5,9 > odu_gzfiles
 
-if [[ -s files_wDiff_sizes ]]
-then 
-        echo -e "\nFiles with different sizes detected. Offending file(s) printed in files_wDiff_sizes. Please check files_wDiff_sizes and compare tamucc_files.txt with current downloaded data"
-	rm tamucc_gzfiles odu_gzfiles
-else 
-	echo -e "\nNo size mismatch in files was detected"
-        rm tamucc_gzfiles odu_gzfiles files_wDiff_sizes
-fi
+#grep -vf tamucc_gzfiles odu_gzfiles > files_wDiff_sizes
+
+#if [[ -s files_wDiff_sizes ]]
+#then 
+#        echo -e "\nFiles with different sizes detected. Offending file(s) printed in files_wDiff_sizes. Please check files_wDiff_sizes and compare tamucc_files.txt with current downloaded data"
+#	rm tamucc_gzfiles odu_gzfiles
+#else 
+#	echo -e "\nNo size mismatch in files was detected"
+#        rm tamucc_gzfiles odu_gzfiles files_wDiff_sizes
+#fi
 
 echo -e "\nIf your download did not work at all, click on the link to the files and visually check in your browser that there is a file named tamucc_files.txt or contact Eric e1garcia@odu.edu"
 
 elif [[ $NCOL -eq 1 ]] 
 then
 # use that list to download all files in parallel.
-cat $OUTDIR/tamucc_files.txt | parallel --no-notice -kj20 wget -c -P $OUTDIR $LINK/{}
+cat $OUTDIR/tamucc_files.txt | parallel --no-notice -kj20 $COMMAND $OUTDIR $LINK/{}
 echo -e "\nFirst download completed.\n\nDownloading again in case partial downloads\n" 
 
 # Second download to check for partial downloads
-cat $OUTDIR/tamucc_files.txt | parallel --no-notice -kj20 wget -c -P $OUTDIR $LINK/{}
+cat $OUTDIR/tamucc_files.txt | parallel --no-notice -kj20 $COMMAND $OUTDIR $LINK/{}
 echo -e "\nSecond download completed" 
 
 # Checking sizes of files from source and lpwd
@@ -73,3 +100,4 @@ echo -e "\nThe tamucc_files.txt does not have file size information (it was crea
 echo -e "\nIf your download did not work at all, click on the link to the files and visually check in your browser that there is a file named tamucc_files.txt"
 fi
 
+echo  -e "\ngridDownloader.sh is done. Please run checkFQ.sh to check the size and format of files"
